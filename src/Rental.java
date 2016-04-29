f44fd0b388ff197d0f0172aa9d9245b6474c3efe
Jan Mannherz <jan.mannherz@online.de>
2016-04-29 12:17:37 +0200

    move getFrequentRenterPoints from Rental to Movies

--- src/Rental.java
+++ src/Rental.java
@@ -13,11 +13,7 @@ class Rental {
     }
 
     int getFrequentRenterPoints() {
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                getDaysRented() > 1)
-            return 2;
-        else
-            return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
     double getCharge() {
         return movie.getCharge(daysRented);
